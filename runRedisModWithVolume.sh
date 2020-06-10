c3aff57befb1e70b3166f516adc1da0cfe9a38ac
Paulo Aboim Pinto <aboimpinto@gmail.com>
2020-06-10 20:30:25 +0200

    remove script files

--- runRedisModWithVolume.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo docker run -v redismodVolume:/data --name apoloRedisModules -p 6379:6379 -d redislabs/redismod
--- runRedisWithVolume.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo docker run -v redisVolume:/data --name apoloRedis -p 6379:6379 -d redis
